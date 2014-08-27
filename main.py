752c51d84e5ee2b9134767fa984439dcf4c17554
Marco Scopesi <marco.scopesi@gmail.com>
2014-08-28 00:37:12 +0200

    Use GPS coordinates to find cities

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ class AddLocationForm(BoxLayout):
         search_url = search_template.format(self.search_input.text)
         request = UrlRequest(search_url, self.found_location)
 
+    def search_coordinates(self):
+        search_template = ("http://api.openweathermap.org/data/2.5/" +
+                           "find?lat={}&lon={}&cnt=10")
+        coordinates = self.search_input.text.split(',')
+        if len(coordinates) == 2:
+            try:
+                lat, lon = float(coordinates[0]), float(coordinates[1])
+                search_url = search_template.format(lat, lon)
+                request = UrlRequest(search_url, self.found_location)
+            except ValueError:
+                pass
+
     def found_location(self, request, data):
         data = json.loads(data.decode()) if not isinstance(data, dict) else data
         if 'list' in data:
